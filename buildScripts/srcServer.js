fd610b3cd00a418e07b2213c7eae7b7bd7d45c0a
Zoltan Bojas <zoltan.bojas@avanade.com>
2018-06-11 15:04:50 -0400

    eslint and eslint-watch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 const port = 3010;
 const app = express();
 const compiler = webpack(config);
